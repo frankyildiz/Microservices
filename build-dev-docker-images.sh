c80b875a7494036e22288f68c56686d4c02bd03b
fatiyildiz <fatihyildiz8935@gmail.com>
2023-10-01 05:07:14 -0700

    Update

--- build-dev-docker-images.sh
+++ build-dev-docker-images.sh
@@ -1,11 +1,11 @@
 ./mvnw clean package
-docker build --force-rm -t "spring-petclinic-admin-server:dev" ./spring-petclinic-admin-server
-docker build --force-rm -t "spring-petclinic-api-gateway:dev" ./spring-petclinic-api-gateway
-docker build --force-rm -t "spring-petclinic-config-server:dev" ./spring-petclinic-config-server
-docker build --force-rm -t "spring-petclinic-customers-service:dev" ./spring-petclinic-customers-service
-docker build --force-rm -t "spring-petclinic-discovery-server:dev" ./spring-petclinic-discovery-server
-docker build --force-rm -t "spring-petclinic-hystrix-dashboard:dev" ./spring-petclinic-hystrix-dashboard
-docker build --force-rm -t "spring-petclinic-vets-service:dev" ./spring-petclinic-vets-service
-docker build --force-rm -t "spring-petclinic-visits-service:dev" ./spring-petclinic-visits-service
-docker build --force-rm -t "spring-petclinic-grafana-server:dev" ./docker/grafana
-docker build --force-rm -t "spring-petclinic-prometheus-server:dev" ./docker/prometheus
+docker build --force-rm -t "microservices-admin-server:dev" ./microservices-admin-server
+docker build --force-rm -t "microservices-api-gateway:dev" ./microservices-api-gateway
+docker build --force-rm -t "microservices-config-server:dev" ./microservices-config-server
+docker build --force-rm -t "microservices-customers-service:dev" ./microservices-customers-service
+docker build --force-rm -t "microservices-discovery-server:dev" ./microservices-discovery-server
+docker build --force-rm -t "microservices-hystrix-dashboard:dev" ./microservices-hystrix-dashboard
+docker build --force-rm -t "microservices-vets-service:dev" ./microservices-vets-service
+docker build --force-rm -t "microservices-visits-service:dev" ./microservices-visits-service
+docker build --force-rm -t "microservices-grafana-server:dev" ./docker/grafana
+docker build --force-rm -t "microservices-prometheus-server:dev" ./docker/prometheus
